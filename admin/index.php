ad1ef688446818b6e801817da273a0121faaf77f
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2020-08-27 17:33:04 +0600

    dash

--- admin/index.php
+++ admin/index.php
@@ -14,12 +14,12 @@
 			<!--header menu start-->
 			<div class="header">
 				<div class="header-menu">
-					<div class="title">Coding <span>Snow</span></div>
+					<div class="logo"><img src="images/logo1.png" alt=""></div>
 					<div class="sidebar-btn">
 						<i class="fas fa-bars"></i>
 					</div>
 					<ul>
-						<li><a href="#"><i class="fas fa-search"></i></a></li>
+						
 						<li><a href="#"><i class="fas fa-bell"></i></a></li>
 						<li><a href="#"><i class="fas fa-power-off"></i></a></li>
 					</ul>
@@ -43,28 +43,38 @@
 							<i class="fas fa-user-circle"></i><span>Profile <i class="fas fa-chevron-down drop-down"></i></span>
 						</a>
 						<div class="sub-menu">
-							<a href="#"><i class="fas fa-image"></i><span>Picture</span></a>
 							<a href="#"><i class="fas fa-address-card"></i><span>Info</span></a>
+							<a href="#"><i class="fas fa-key"></i><span>Password</span></a>
 						</div>
 					</li>
 					<li class="item" id="messages">
 						<a href="#messages" class="menu-btn">
-							<i class="fas fa-envelope"></i><span>Messages <i class="fas fa-chevron-down drop-down"></i></span>
+							<i class="fas fa-calendar"></i><span> Appointment<!--<i class="fas fa-chevron-down drop-down"></i>--></span>
 						</a>
-						<div class="sub-menu">
+						<!--<div class="sub-menu">
 							<a href="#"><i class="fas fa-envelope"></i><span>New</span></a>
 							<a href="#"><i class="fas fa-envelope-square"></i><span>Sent</span></a>
 							<a href="#"><i class="fas fa-exclamation-circle"></i><span>Spam</span></a>
-						</div>
+						</div>-->
 					</li>
 					<li class="item" id="settings">
 						<a href="#settings" class="menu-btn">
-							<i class="fas fa-cog"></i><span>Settings <i class="fas fa-chevron-down drop-down"></i></span>
+							<i class="fas fa-user-md"></i><span>Doctor <!--<i class="fas fa-chevron-down drop-down"></i>--></span>
 						</a>
-						<div class="sub-menu">
+						<!--<div class="sub-menu">
 							<a href="#"><i class="fas fa-lock"></i><span>Password</span></a>
 							<a href="#"><i class="fas fa-language"></i><span>Language</span></a>
-						</div>
+						</div>-->
+					</li>
+					<li class="item">
+						<a href="#" class="menu-btn">
+							<i class="fas fa-user"></i><span>Helpline Assistant</span>
+						</a>
+					</li>
+					<li class="item">
+						<a href="#" class="menu-btn">
+							<i class="fas fa-tint"></i><span>Organ Donation</span>
+						</a>
 					</li>
 					<li class="item">
 						<a href="#" class="menu-btn">
